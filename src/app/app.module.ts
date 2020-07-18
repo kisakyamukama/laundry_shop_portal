b4d7a637e495597e485e912c67acd0c79562a9e2
Kabali Michael <fitzerkisakyamukama03@gmail.com>
2020-07-18 20:51:39 +0300

    add bootstrap font-awesome and create customer, user and record modules and their respective components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { CustomerModule } from './customer/customer.module';
+import { RecordModule } from './record/record.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule,
+    CustomerModule,
+    RecordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/record/record.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecordComponent } from './record.component';
+
+
+
+@NgModule({
+  declarations: [RecordComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RecordModule { }
